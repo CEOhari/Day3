f439311ef23f7181404cdf52cca631705620664d
Hariharan <srihariharan040698@gmail.com>
2024-06-25 17:12:11 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,22 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> s="example_string"
+>>> first_five=s[:5]
+>>> print(first_five)
+examp
+>>> #extract and print the last three characters of a string
+>>> last_three=s[-3:]
+>>> print(last_three)
+ing
+>>> #extract and print the middle section of a string:
+>>> middle_section=s[4:8]
+>>> print(middle_section)
+ple_
+>>> #reverse a string and print the result:
+>>> reversed_string=s[::-1]
+>>> print(reversed_string)
+gnirts_elpmaxe
+>>> #extract and print every second character from a string:
+>>> every_second_char=s[1::2]
+>>> print(every_second_char)
+xml_tig
